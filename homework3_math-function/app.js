f492726a76b10a3406fecf18905c3e70b1731a27
NazarShevchuk <sevcuk031@gmail.com>
2022-08-12 23:58:01 +0300

    homework3

---
+++ homework3_math-function/app.js
@@ -0,0 +1,36 @@
+const askNumbers = (num1, num2, oper) => {
+    const firstNumber = num1  Number(prompt("Введите первое число"));
+    const secondNumber = num2  Number(prompt("Введите второе число"));
+    const operation = oper  prompt("Введите знак операции");
+
+
+
+    if(Number.isNaN(firstNumber)  Number.isNaN(secondNumber)) {
+        return askNumbers(firstNumber, secondNumber, operation)
+    }
+
+    return {firstNumber, secondNumber,operation}
+}
+
+
+const calcResult = (firstNumb, secondNumb, operation) => {
+  switch (operation) {
+    case "+":
+      return firstNumb + secondNumb;
+    case "-":
+      return firstNumb - secondNumb;
+    case "*":
+      return firstNumb * secondNumb;
+    case "/":
+      return firstNumb / secondNumb;
+    default:
+      return null;
+  }
+};
+
+const {firstNumber, secondNumber, operation} =  askNumbers()
+
+const answer = calcResult(firstNumber, secondNumber, operation)
+
+alert(answer)
+console.log(answer);
