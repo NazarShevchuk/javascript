cec2cec04c6eae9032aaec1f49eebea6c68de4cd
NazarShevchuk <sevcuk031@gmail.com>
2022-08-12 23:59:13 +0300

    fixhomework3

--- homework3_math-function/app.js
+++ homework3_math-function/app.js
@@ -1,17 +1,14 @@
 const askNumbers = (num1, num2, oper) => {
-    const firstNumber = num1  Number(prompt("Введите первое число"));
-    const secondNumber = num2  Number(prompt("Введите второе число"));
-    const operation = oper  prompt("Введите знак операции");
+  const firstNumber = num1 || Number(prompt("Введите первое число"));
+  const secondNumber = num2 || Number(prompt("Введите второе число"));
+  const operation = oper || prompt("Введите знак операции");
 
+  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
+    return askNumbers(firstNumber, secondNumber, operation);
+  }
 
-
-    if(Number.isNaN(firstNumber)  Number.isNaN(secondNumber)) {
-        return askNumbers(firstNumber, secondNumber, operation)
-    }
-
-    return {firstNumber, secondNumber,operation}
-}
-
+  return { firstNumber, secondNumber, operation };
+};
 
 const calcResult = (firstNumb, secondNumb, operation) => {
   switch (operation) {
@@ -28,9 +25,9 @@ const calcResult = (firstNumb, secondNumb, operation) => {
   }
 };
 
-const {firstNumber, secondNumber, operation} =  askNumbers()
+const { firstNumber, secondNumber, operation } = askNumbers();
 
-const answer = calcResult(firstNumber, secondNumber, operation)
+const answer = calcResult(firstNumber, secondNumber, operation);
 
-alert(answer)
+alert(answer);
 console.log(answer);
